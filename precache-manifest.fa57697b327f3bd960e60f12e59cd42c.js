e79aeb44130aac6306162ea4f20b10c73625a769
anthonysea <anthonyqchao@gmail.com>
2020-08-25 17:19:51 -0700

    Updates

---
+++ precache-manifest.fa57697b327f3bd960e60f12e59cd42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a157acb56ff1a5b6d7b0da25a13a4d08",
+    "url": "/robot-rolodex/index.html"
+  },
+  {
+    "revision": "f0c3d3df3e461094035d",
+    "url": "/robot-rolodex/static/css/main.b7c06714.chunk.css"
+  },
+  {
+    "revision": "bdfd07a304833eead0ff",
+    "url": "/robot-rolodex/static/js/2.7c4fadac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robot-rolodex/static/js/2.7c4fadac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c3d3df3e461094035d",
+    "url": "/robot-rolodex/static/js/main.0d3e4798.chunk.js"
+  },
+  {
+    "revision": "4ccae0a1c6e2ecd2f19e",
+    "url": "/robot-rolodex/static/js/runtime-main.08f66452.js"
+  }
+]);
